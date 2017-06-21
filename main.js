8129a415c0610ac2c1a1cdf00a069f433627807b
wizardsambolton <sam99bolton@gmail.com>
2017-06-21 14:17:22 +0100

    add room model

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-mongoose = require("mongoose");
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test');
+var Room = mongoose.model({
+    "id": String,
+	"title": String,
+	"area": String,
+	"light": Boolean,
+	"content": String,
+	"terrain": String,
+	"outdoors": String,
+	"waitMod": Number,
+	"moveMod": Number,
+	"exits": Array,
+	"playersInRoom": Array,
+	"monsters": Array,
+	"items": Array,
+	"itemType": String,
+	"hasEvents": Boolean,
+	"preventRecall": Boolean,
+	"preventDecay": Boolean,
+	"behaviors": Array
+})
