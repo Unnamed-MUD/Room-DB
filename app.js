e7c83ac0e901f165389671eb6f5fc81ae10a6dbf
Jack Perkins <jackaperkins@gmail.com>
2017-06-22 15:34:13 +0200

    add blobs type to db for test example

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var blobs = require('./routes/blobs');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(blobs); // dont need a string ahead of the route name
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
