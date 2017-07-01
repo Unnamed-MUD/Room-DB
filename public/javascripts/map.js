f48c627473487acf35c3059f9781c444f7a7c6ca
Jack Perkins <jackaperkins@gmail.com>
2017-07-01 14:16:42 +0200

    improve map to show 2-way connections, prepare for merge with editor

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,6 +8,9 @@ var roomsList = [];
 
 var ctx;
 
+var width = 400;
+var height = 600;
+
 var goalDistance = 100;
 var safeDistance = 120;
 
@@ -42,36 +45,42 @@ function draw() {
 
   ctx.fillStyle = 'rgb(240, 240, 255)';
   ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-  ctx.fillRect(0,0, 800,600);
-
-
-  roomsList.forEach(function(room) {
-    GoRoom(room);
-  });
+  ctx.fillRect(0,0, width, height);
 
   RepulseRooms();
+
+  roomsList.forEach(function(room) {
+    DrawRoomBox(room);
+  });
+  roomsList.forEach(function(room) {
+    UpdateRoom(room);
+  });
+
   //  ctx.fillRect(100,199 ,30, 30);
   window.requestAnimationFrame(draw);
 }
 
 function SetupRoom (room) {
-  room.x = Math.random() * 300 + 200;
-  room.y = Math.random() * 200 + 100;
+  room.x = Math.random() * 200 + width / 2 - 100;
+  room.y = Math.random() * 200 + width / 2 - 100;
   room.dx = room.x;
   room.dy = room.y;
 }
 
-function GoRoom (room) {
-  ctx.strokeStyle = 'rgb(0, 0, 0)';
-  room.x += (Math.random() - 0.5) * 0.05;
-  room.y += (Math.random() - 0.5) * 0.05;
-
+function DrawRoomBox (room) {
+  ctx.strokeStyle = 'rgb(0,0,0)';
   ctx.fillStyle='rgb(0,0,0)';
   ctx.fillText(room.title,room.x,room.y -25);
   ctx.fillStyle='rgb(255,255,255)';
   ctx.fillRect(room.x- 20, room.y -20,40, 40);
   ctx.strokeRect(room.x- 20, room.y -20,40, 40);
+}
 
+function UpdateRoom (room) {
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'rgb(0, 0, 0)';
+  room.x += (Math.random() - 0.5) * 0.05;
+  room.y += (Math.random() - 0.5) * 0.05;
 
   room.exits.forEach(function(exit) {
     target = FindRoom(exit.room);
@@ -82,7 +91,15 @@ function GoRoom (room) {
       Move (room, angle, change * 0.5);
       Move (target, angle, change * -0.5);
 
+      if(ExitInRoom(room._id, target)) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = 'rgb(0,0,0)';
+      } else {
+          ctx.strokeStyle = 'rgb(255,0,0)';
+        ctx.lineWidth= 1;
+      }
       DrawLine(room.x, room.y, target.x, target.y);
+
     }
   });
 
@@ -93,8 +110,8 @@ function GoRoom (room) {
   room.dx = tempX;
   room.dy = tempY;
 
-  room.x = Math.max(50, Math.min(750, room.x));
-  room.y = Math.max(50, Math.min(650, room.y));
+  room.x = Math.max(25, Math.min(width - 25, room.x));
+  room.y = Math.max(25, Math.min(height -25, room.y));
 }
 
 function RepulseRooms () {
@@ -105,12 +122,10 @@ function RepulseRooms () {
       if(room1 != room2) {
         var dis = Distance(room1, room2);
         if(dis < safeDistance) {
-          var change = (safeDistance -dis) * 0.005;
+          var change = (safeDistance -dis) * 0.002;
           var angle = AngleTo(room2, room1);
           Move (room1, angle, change * 0.5);
           Move (room2, angle, change * -0.5);
-        //  ctx.strokeStyle = 'rgba(255, 0, 0, 0.4)';
-      //    DrawLine(room1.x, room1.y, room2.x + Math.random()*4, room2.y+ Math.random()*4);
         }
       }
     }
@@ -151,4 +166,15 @@ function AngleTo(room1, room2) {
 function Move(room, angle, distance) {
   room.x += Math.cos(angle) * distance;
   room.y += Math.sin(angle) * distance;
+}
+
+function ExitInRoom (id, room) {
+  for(var i = 0; i < room.exits.length; i++) {
+    var exit = room.exits[i];
+  //  console.log(exit.room + " " + id + ((exit.room == id) ? ' TRUEEEEEE' : ''));
+    if(exit.room == id) {
+      return true;
+    }
+  }
+  return false;
 }
